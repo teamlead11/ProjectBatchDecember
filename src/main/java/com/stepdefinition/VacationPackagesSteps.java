905ab9d5a0b2b36b002160f1137dfcb15019c076
raaja <raajasekararunachalam@gmail.com>
2018-01-31 16:02:31 +0530

    Test Script for Minimum Price

---
+++ src/main/java/com/stepdefinition/VacationPackagesSteps.java
@@ -0,0 +1,59 @@
+package com.stepdefinition;
+
+import org.openqa.selenium.By;
+
+import com.objectRepository.VacationPackages;
+import com.resources.FunctionalLibrary;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class VacationPackagesSteps extends FunctionalLibrary {
+	@Given("^I am on delta login page$")
+	public void i_am_on_delta_login_page() throws Throwable {
+		driver.get("https://www.delta.com");
+	}
+
+	@When("^I enter the valid details$")
+	public void i_enter_the_valid_details() throws Throwable {
+		VacationPackages v = new VacationPackages();
+		click(v.getLnkVacation());
+		click(v.getCloseBtn());
+		setText(v.getTxtFromLocation(), "LAX");
+		selectByIndex(v.getTxtFromLocation(), 0);
+		setText(v.getTxtToLocation(), "cal");
+		selectByValue(v.getTxtToLocation(), "YYC");
+		setText(v.getDepartureDate(), "02/01/2018");
+		setText(v.getReturnDate(), "02/15/2018");
+		click(v.getDrpdwnAdult());
+		click(v.getBtnSubmit());
+	}
+
+	@When("^Check wether the application navigates to next page$")
+	public void check_wether_the_application_navigates_to_next_page() throws Throwable {
+		VacationPackages v = new VacationPackages();
+		Thread.sleep(2500);
+		v.sortingTheHotel("price");
+		click(v.getBtnSelect());
+		click(v.getLoadBtn());
+		Thread.sleep(5000);
+		v.sortingOutboundFlight("price");
+		Thread.sleep(1500);
+		click(v.getBtnSelectOutboundFlight());
+		click(v.getLoadBtn());
+		Thread.sleep(5000);
+		v.sortingOutboundFlight("price");
+		Thread.sleep(1500);
+		click(v.getBtnSelectOutboundFlight());
+		click(v.getLoadBtn());
+		v.rentingCarBasedOnPrice("Intermediate car");
+		click(driver.findElement(By.xpath("(//button[text()='ADD'])[3]")));
+
+	}
+
+	@Then("^I verify all the details$")
+	public void i_verify_all_the_details() throws Throwable {
+	}
+
+}
---
+++ src/test/resources/com/Rajasekar/Vacation.feature
@@ -0,0 +1,7 @@
+Feature: Verify wether user can able to access the vacation package module
+
+Scenario: Verify wether user can able to access the vacation package module
+Given I am on delta login page
+When I enter the valid details 
+And Check wether the application navigates to next page
+Then I verify all the details
